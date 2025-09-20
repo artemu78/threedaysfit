65e24289f67ca958b200d1040ac12fe7d9aa5fd8
Artem Reva <artemreva79@gmail.com>
2025-09-20 16:30:16 +0300

    fix broken import in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,7 @@ import { Router } from "@/components/router";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -23,12 +23,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 
 function App() {
-
-
-
   const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
   if (!clientId) {
     throw new Error("Missing Google Client ID");
