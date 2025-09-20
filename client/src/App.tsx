66b3b2c3f4c33651d693605a1caf80c9cf8a2aae
Artem Reva <artemreva79@gmail.com>
2025-09-20 16:25:58 +0300

    Fix transformToFirestoreFormat, remove sensitive data from user storage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,28 @@ import Footer from "@/components/footer";
 import { Router } from "@/components/router";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBc4Op3ol10fColdBsOvLhMWFnHecwvw3k",
+  authDomain: "days-fit-471521.firebaseapp.com",
+  projectId: "days-fit-471521",
+  storageBucket: "days-fit-471521.firebasestorage.app",
+  messagingSenderId: "107916257043",
+  appId: "1:107916257043:web:a3da5ec4a59bbefc149ce6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 function App() {
+
+
+
   const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
   if (!clientId) {
     throw new Error("Missing Google Client ID");
